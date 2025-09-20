c83bd8669223cf405a6308c6fffdf64b87b83bc0
enosco <scottrweno@gmail.com>
2025-09-20 19:58:39 -0400

    Fixed color matching, added documentation

--- src/cursedvga.c
+++ src/cursedvga.c
@@ -7,111 +7,127 @@
 #define CHARS_PER_PIXEL 2
 #define COLORSCALE (1000.0 / 255.0)
 
+/*
+ * Header of a TARGA file in order of each field
+ */
 typedef struct __attribute__((packed)) {
-    char   id_len;
-    char   color_map_type;
-    char   image_type;
-    short  color_map_origin;
-    short  color_map_len;
-    char   color_map_entry_size;
-    short  x_orig;
-    short  y_orig;
-    short  width;
-    short  height;
-    char   pixel_depth;
-    char   img_desc_byte;
+    uint8_t   id_len;
+    uint8_t   color_map_type;
+    uint8_t   image_type;
+    uint16_t  color_map_origin;
+    uint16_t  color_map_len;
+    uint8_t   color_map_entry_size;
+    uint16_t  x_orig;
+    uint16_t  y_orig;
+    uint16_t  width;
+    uint16_t  height;
+    uint8_t   pixel_depth;
+    uint8_t   img_desc_byte;
 } TARGA_HEADER;
 
+/*
+ * RGB values of a single pixel in BGR order to match
+ * order and bits used by a TARGA file using 24-bit color.
+ */
 typedef struct __attribute__((packed)) {
     uint8_t blue_val;
     uint8_t green_val;
     uint8_t red_val;
 } PIXEL;
 
+/*
+ * Information given by a single pixel packet, including
+ * its type and the number of pixels it affects.
+ */
 typedef struct {
     enum {RUNLEN, RAW} type;
     int count; // either repetition count or number of pixels in packet depending on type
 } PACKET_INFO;
 
-typedef struct __attribute__((packed)) {
-    int repetition_count;
-    PIXEL color_data;
-} RUN_LEN_PACKET_DATA;
-
-int is_gray(int r, int g, int b) {
-    return -1;
+/*
+ * Returns 1 if the given red, green, and blue are all the same value
+ * AND if said values are neither perfect black (0,0,0)  or white (255,255,255).
+ * Returns 0 otherwise
+ */
+int is_gray(int r, int g, int b) {    
+    return r == g && r == b && b == g
+	&& ( r != 0 && r != 255);
 }
 
-int get_nearest_color_pair(PIXEL *palette, int red, int green, int blue, WINDOW* log_win) {
+/*
+ * Returns the color pair of the closest match to the given rgb value.
+ * Assumes that the supplied palette has already been filled by the
+ * generate_palette() function.  
+ */
+int get_nearest_color_pair(PIXEL *palette, int red, int green, int blue) {
     int palette_index = 0;
     int ceil, floor = 0;
     
-//    int red_ceil, red_floor = 0;
-    do {
-	floor = palette[palette_index].red_val;	
-	ceil = palette[palette_index + 40].red_val;
-	palette_index += 40; // 40 cells per table
-    } while (ceil < red && palette_index < (5 * 40));
+    if (is_gray(red, green, blue)) {
+	palette_index = 240;
+
+	do {
+	    floor = palette[palette_index].red_val;	
+	    ceil = palette[palette_index + 1].red_val;
+	    palette_index++; // 40 cells per table
+	} while (ceil < red && palette_index < 256);
+	
+	palette_index = ((red - floor) < (ceil - red))
+	    ? palette_index - 1
+	    : palette_index;
+	
+    } else {
+	
+	do {
+	    floor = palette[palette_index].red_val;	
+	    ceil = palette[palette_index + 40].red_val;
+	    palette_index += 40; // 40 cells per table
+	} while (ceil < red && palette_index < (5 * 40));
+	
+	palette_index = ((red - floor) < (ceil - red))
+	    ? palette_index - 40
+	    : palette_index;
+
+	ceil = 0;
+	floor = 0;    
+	do {
+	    floor = palette[palette_index].green_val;	
+	    ceil = palette[palette_index + 6].green_val;
+	    palette_index += 5; // 5 cells per row
+	} while (ceil < green && palette_index < (6 * 40));
     
-//    int ceil_diff = red_ceil - red;
-//    int floor_diff = red - red_floor;
-    
-//    palette_index = (floor_diff < ceil_diff) ? palette_index - 40 : palette_index;
-    palette_index = ((ceil - red) < (red - floor))
-		      ? palette_index - 40
-		      : palette_index;
+	palette_index = ((green - floor) < (ceil - green))
+	    ? palette_index - 6
+	    : palette_index;
 
-    ceil = 0;
-    floor = 0;    
-    do {
-	floor = palette[palette_index].green_val;	
-	ceil = palette[palette_index + 6].green_val;
-	palette_index += 5; // 5 cells per row
-    } while (ceil < green && palette_index < (5 * 40));
+	ceil = 0;
+	floor = 0;    
+	do {
+	    floor = palette[palette_index].blue_val;
+	    ceil = palette[palette_index + 1].blue_val;
+	    palette_index++; // 1 cell per column
+	} while (ceil < blue && palette_index < (6 * 40));
 
-    palette_index = ((ceil - green) < (green - floor))
-		      ? palette_index - 6
-		      : palette_index;
+	palette_index = ((blue - floor) < (ceil - blue))
+	    ? palette_index - 1
+	    : palette_index;
+    }
 
-    ceil = 0;
-    floor = 0;    
-    do {
-	floor = palette[palette_index].blue_val;
-	ceil = palette[palette_index + 1].blue_val;
-	palette_index++;; 
-    } while (ceil < blue && palette_index < (5 * 40));
-
-    palette_index = ((ceil - blue) < (blue - floor))
-		      ? palette_index - 1
-		      : palette_index;
-
-		      
-    /*
-    printf("ceil: %d, floor: %d, for red val: %d\n\r", red_ceil, red_floor, red);
-    printf("ceil diff: %d, floor diff: %d\n\r", ceil_diff, floor_diff);
-    printf("closest: %d at idx %d", palette[palette_index].red_val, palette_index);
-    */
-        
     return palette_index;
 }
 
-int compare_rgb(const void* a, const void* b) {
-    PIXEL* p1 = (PIXEL*) a;
-    PIXEL* p2 = (PIXEL*) b;
-    
-    int red_diff = p1->red_val - p2->red_val;
-    if (red_diff != 0) {
-	return red_diff;
-    }
-    
-    int green_diff = p1->green_val - p2->green_val;
-    if (green_diff != 0) {
-	return green_diff;
-    }
-
-    return p1->blue_val - p2->blue_val;
-}
-
+/*
+ * Fills the supplied PIXEL array with an 8-bit color palette.
+ * First 240 indicies represent a 6x8x5 color cube while remaining
+ * 16 indicies are grayscale.
+ *
+ * Red can be iterated through by traversing in intervals of 40 indicies,
+ * Green by intervals of 5,
+ * Blue by intervals of 1  
+ *
+ * Grayscale can be iterated through by jumping directly to index 240 and
+ * continuing until 255.
+ */
 void generate_palette(PIXEL *palette) {
     int color_num = 0;
     int pair_num = 0;
@@ -150,10 +166,7 @@ void generate_palette(PIXEL *palette) {
 	    .blue_val = i
 	};	
     }
-
-    //qsort(palette, 256, sizeof(PIXEL), compare_rgb);
     
-    int c = 0;
     for (int i = 0; i < 256; i++) {
 	init_color(i,
 		   palette[i].red_val * COLORSCALE,
@@ -161,22 +174,8 @@ void generate_palette(PIXEL *palette) {
 		   palette[i].blue_val * COLORSCALE);	          
 	
 	init_pair(i, COLOR_BLACK, i);
-
-	attron(COLOR_PAIR(i));
-	printw("%02x%02x%02x ", palette[i].red_val, palette[i].green_val, palette[i].blue_val);
-//	printw("     ", palette[i].red_val, palette[i].green_val, palette[i].blue_val);
-	
-	refresh();
-	attroff(COLOR_PAIR(i));
-
-	c++;
-	if (c == 5) {
-	    printw("\n");
-	    c = 0;
-	}
     }
 
-    getchar();
     erase();
     refresh();
 }
@@ -185,35 +184,7 @@ void setup_ncurses(WINDOW* win, int height, int width) {
 
 }
 
-// Initializes color with given rgb values and
-// returns corresponding color pair
-int initialize_color(int r, int g, int b) {
-    static int color_pair_num = 2;
-    static int color_num = 8;
-    
-    if (color_num > COLORS) {
-	return -1;
-    }
-
-    if (color_pair_num > COLOR_PAIRS) {
-	return -2;
-    }
-    
-    init_color(color_num,
-	       r * COLORSCALE,
-	       g * COLORSCALE,
-	       b * COLORSCALE);
-    
-    init_pair(color_pair_num, COLOR_BLACK, color_num);
-
-    int pair_to_return = color_pair_num;
-    color_num++;
-    color_pair_num++;
-
-    return pair_to_return;
-}
-
-/* changes the next attributes of next 2 pixels to
+/* Changes the next attributes of next 2 pixels to
  * color at color_pair and updates the cursor
  *
  * returns true once the last pixel of the image has been placed
@@ -228,7 +199,7 @@ int set_color_and_update_cursor(WINDOW* img_win, int color_pair, int pixels_affe
     // TODO: might need to handle case where run-length packet
     // overflows across line
     wchgat(img_win, cells_affected, 0, color_pair, NULL);
-    usleep(10000);
+    usleep(10);
 
     // adjust cursor and move to next row if x_pos exceeds image width    
     x_pos += cells_affected;
@@ -252,8 +223,7 @@ int display_run_length_packet(WINDOW* img_win, FILE* file, int repetition_count,
     int pair_num = get_nearest_color_pair(palette,
 					  pixel.red_val,
 					  pixel.green_val,
-					  pixel.blue_val,
-					  NULL);
+					  pixel.blue_val);
 
     int8_t ret = set_color_and_update_cursor(img_win, pair_num, repetition_count);
     wrefresh(img_win);
@@ -267,10 +237,9 @@ int display_raw_rgb_packet(WINDOW* img_win, FILE *file, int pixel_count, PIXEL* 
 	fread(&pixel, sizeof(uint8_t), sizeof(PIXEL), file);
 
 	int pair_num = get_nearest_color_pair(palette,
-					  pixel.red_val,
-					  pixel.green_val,
-					  pixel.blue_val,
-					  NULL);
+					      pixel.red_val,
+					      pixel.green_val,
+					      pixel.blue_val);
 
 	ret = set_color_and_update_cursor(img_win, pair_num, 1);
 	usleep(5000);
@@ -345,7 +314,7 @@ int main(int argc, char* argv[]) {
     generate_palette(palette);
 
 //    get_nearest_color_pair(palette, 2, 0, 0, NULL);
-    getchar();
+//    getchar();
     
     init_pair(0, 0, 0);
     attron(COLOR_PAIR(1));
